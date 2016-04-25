2e103b8f653be0d4d27f38227432c5575c4d77fd
lolopaluza <upsonmanoah@gmail.com>
2016-04-25 16:26:52 +0200

    first commit to github

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+Task.create([
+  { title: "Seed the the database", completed: true },
+  { title: "Create a tasks controller" },
+  { title: "Create routing" },
+  { title: "Print out some JSON I guess?" }
+])
+
+puts "Seeded #{Task.count} Tasks!"
